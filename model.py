9a656314bbf784cb2da85e96230178d8485c3926
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2019-08-26 13:13:25 +0200

    Har lavet __init__, shiftUp og shiftDown

--- model.py
+++ model.py
@@ -1,16 +1,38 @@
 from random import randint
 
 class Car(object):
-    pass
+	pass
 
 class Wheel(object):
-    pass
+	pass
 
 class Engine(object):
-    pass
+	pass
 
 class Gearbox(object):
-    pass
+	def __init__(self):
+		self.wheels = dict(frontLeft=Wheel(), frontRight=Wheel(), rearLeft=Wheel(), rearRight=Wheel())
+		self.currentGear = 0
+		self.clutchEngaged = False
+		self.gears = [0, 0.8, 1, 1.4, 2.2, 3.8]
+		
+	def shiftUp(self):
+		if self.currentGear == (len(self.gears)-1) or self.clutchEngaged == True:
+			pass
+		else:
+			self.currentGear += 1
+	
+	def shiftDown(self):
+		if self.currentGear == 0 or self.clutchEngaged == True:
+			pass
+		else:
+			self.currentGear -= 1
+	pass
 
 class Tank(object):
-    pass
+	pass
+
+hej=Gearbox()
+
+hej.shiftUp()
+print(str(hej.currentGear))
