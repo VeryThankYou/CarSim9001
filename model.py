d0323006518d61bec83419fc21a32c7b02f78fe3
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2019-08-28 13:51:14 +0200

    Har lavet engine og wheels

--- model.py
+++ model.py
@@ -4,9 +4,29 @@ class Car(object):
 	pass
 
 class Wheel(object):
+	def __init__(self):
+		self.orientation = randint(0, 360)
+	
+	def rotate(self, revolutions):
+		self.orientation = (self.orientation + int(revolutions)*360) %360
 	pass
 
 class Engine(object):
+	def __init__(self):
+		self.throttlePosition = 0
+		self.theGearbox = Gearbox()
+		self.currentRpm = 0
+		self.consumptionConstant = 0.0025
+		self.maxRpm = 100
+		self.theTank = Tank()
+		
+	def updateModel(self, dt):
+		if self.theTank.capacity > 0:
+			self.currentRpm = self.throttlePosition*self.maxRpm
+			self.theTank.capacity.remove(self.currentRpm*self.consumptionConstant)
+			self.theGearbox.rotate(currentRpm*(dt/60))
+		else:
+			currentRpm = 0
 	pass
 
 class Gearbox(object):
@@ -27,12 +47,15 @@ class Gearbox(object):
 			pass
 		else:
 			self.currentGear -= 1
+	
+	def rotate(self, revolutions):
+		if self.clutchEngaged == True:
+			wheelRotations = int(revolutions)*self.gears[self.currentGear]
+			for e in self.wheels:
+				self.wheels[e].rotate(wheelRotations)
 	pass
 
 class Tank(object):
 	pass
 
-hej=Gearbox()
 
-hej.shiftUp()
-print(str(hej.currentGear))
